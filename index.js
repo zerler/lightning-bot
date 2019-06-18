65ecc31a05776f5fd1cbca77cd938bfc331b7ad3
Ben Zerler <zerler@wisc.edu>
2019-06-17 20:16:06 -0500

    im a dummy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ http        = require('http');
 director    = require('director');
 cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
-survive     = require('./nosleep.js');
+nosleep     = require('./nosleep.js');
 
 router = new director.http.Router({
   '/' : {
