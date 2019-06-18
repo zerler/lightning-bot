30343d112ba373a34b94d48c7c466c6b43c0b267
Ben Zerler <zerler@wisc.edu>
2019-06-18 16:02:10 -0500

    i'm REALLY dumb

--- nosleep.js
+++ nosleep.js
@@ -3,7 +3,7 @@ var http = require("http");
 const survive = () => {
     const now = new Date();
 
-    if (now.getHours() < 3 || now.getHours > 13){
+    if (now.getHours() < 3 || now.getHours() > 13){
         http.get("http://zerler-lightning-bot.herokuapp.com");
         console.log('keeping myself alive :)');
     } else {
